f51db40571dab7d4a199edb834424e951b35c922
Mapu456 <mpaula456@hotmail.com>
2021-11-29 10:57:24 -0500

    points 8 and 10 even numbers

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - function that measures the depth of a node in a binary tree
+ * @tree: pointer to the root node of the tree to traverse
+ * Return: node is NULL, return 0
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+
+}
