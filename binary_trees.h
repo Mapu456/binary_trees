1b762523ea30cba07a9deac30a35dd445f407c53
Laurajcb <3398@holbertonschool.com>
2021-11-29 22:48:04 -0500

    ADDED and update point 3

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,8 @@
 #ifndef BINARY_TREES
 #define BINARY_TREES
 
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -29,6 +31,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
