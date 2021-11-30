218da02ca75b884b923e2ef6053e067f57286478
Laurajcb <3398@holbertonschool.com>
2021-11-30 08:28:55 -0500

    ADDED more points and update header

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	if (parent->left)
 	{
-		new_node = parent->left;
+		new_node->left = parent->left;
 		new_node->left->parent = new_node;
 	}
 	new_node->parent->left = new_node;
