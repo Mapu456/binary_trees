80a34a5f0c487fc94e3afd5e3d2ba39c78eef47e
Laurajcb <3398@holbertonschool.com>
2021-12-01 12:26:49 -0500

    fix code point 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,26 +7,19 @@
  * binary_tree_leaves - counts the leaves in a binary tree
  * @tree: pointer to the root node of the tree to traverse
  * Return: node is NULL, return 0
+ * A NULL pointer is not a leave
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	int i = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	if (tree->right != NULL)
-	{
-		i += 1;
-	}
-	if (tree->left != NULL)
-	{
-		i += 1;
-	}
-	if (tree->right == NULL || tree->left == NULL)
-	{
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
+
+	else
+	{
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 	}
-	return (i);
+	return (0);
 }
